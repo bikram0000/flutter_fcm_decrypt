b69fa9621d0f028241170084466f2a3c2d69ec68
Bikramaditya <bikram.magnigeeks@gmail.com>
2024-04-22 11:06:04 +0530

    get package path from package config to use in both pure dart and flutter

--- lib/flutter_fcm_decrypt.dart
+++ lib/flutter_fcm_decrypt.dart
@@ -1,12 +1,11 @@
-
 import 'dart:async';
 import 'dart:ffi';
 import 'dart:io';
 import 'dart:isolate';
 import 'package:ffi/ffi.dart';
+import 'package:package_config/package_config.dart';
 
 import 'flutter_fcm_decrypt_bindings_generated.dart';
-import 'dart:ffi' as ffi;
 
 /// A very short-lived native function.
 ///
@@ -14,14 +13,13 @@ import 'dart:ffi' as ffi;
 /// They will block the Dart execution while running the native function, so
 /// only do this for native functions which are guaranteed to be short-lived.
 void main() {
-
   decryptMessage(
-      '{"object":{"id":"B4362F8D","from":"BDOU99-h67HcA6JeFXHbSNMu7e2yNNu3RzoMj8TM4W88jITfq7ZmPvIM1Iv-4_l2LxQcYwhqby2xGpWwzjfAnG4","category":"org.chromium.linux","appData":[{"key":"google.source","value":"webpush"},{"key":"encryption","value":"salt=2-XbF7yVGnxuu6xf2HCWrg=="},{"key":"subtype","value":"wp:receiver.push.com#7c47ef8f-ad1d-45c0-b048-e37b8d95f092"},{"key":"crypto-key","value":"dh=BBchCtcAXtA-ghgjOPK_I9I3JQ6A3Huj58HHwq4ScfLiQWKhWDe7qPinGsNfdV31kNfN3e_-UvX28Wcvpqk0JBg="}],"persistentId":"0:1711623408410794%7031b2e6f9fd7ecd","ttl":2419200,"sent":"1711623408408","rawData":[144,100,24,211,44,137,162,184,201,92,144,234,7,197,77,100,149,56,136,47,103,51,248,229,49,83,203,100,91,191,11,118,151,111,8,55,72,14,228,148,33,72,59,211,84,32,112,20,184,10,11,117,105,172,109,21,163,165,149,84,19,5,18,7,105,183,142,104,117,239,128,161,91,56,75,226,113,36,1,140,53,168,206,233,83,118,186,247,86,217,163,59,57,183,47,194,105,155,78,88,176,203,166,85,189,101,158,207,145,115,178,125,3,150,177,90,107,58,247,207,174,225,24,81,255,198,170,76,88,3,193,213,204,173,241,188,189,165,32,250,205,243,75,195,200,166,63,52,253,199,109,18,21,30,145,50,220,163,34,19,152,148,190,75,234,218,135,238,200,253,113,100,172,46,225,22,189,246,221,5,48,188,238,208,34,145,104,91,225,200,112,59,227,155,184,163,37,103,48,161,164,72,71,49,136,89,218,122,139,20,43,120,163,161,175,136,89,152,67,148,198,7,83,107,134,132,176,203,239,178,247,178,153,110,214,223,237,251,20,184,125,243,18,8,40,105,135,133,51,190,243,0,160,218,12,150,219,10,95,67,37,25,192,43,245]},"keys":{"privateKey":"mxS1cWvYGFxJJS8RTsOFkW5VqRyL4k6mIH0K3V47aHQ","publicKey":"BF4--ivRFQqtqhVlEbK6HoBlznFZu9lcAwpps-IDwTpuzLgxUQkRpMsxTnOUUHdbZK4XmFFo3c4ArvxXOcJHDPc","authSecret":"UG5PMDBLZkw5WHVFWmp0Vw"}}').then((value) {
-        print("resutlt  :: $value");
+          '{"object":{"id":"B4362F8D","from":"BDOU99-h67HcA6JeFXHbSNMu7e2yNNu3RzoMj8TM4W88jITfq7ZmPvIM1Iv-4_l2LxQcYwhqby2xGpWwzjfAnG4","category":"org.chromium.linux","appData":[{"key":"google.source","value":"webpush"},{"key":"encryption","value":"salt=2-XbF7yVGnxuu6xf2HCWrg=="},{"key":"subtype","value":"wp:receiver.push.com#7c47ef8f-ad1d-45c0-b048-e37b8d95f092"},{"key":"crypto-key","value":"dh=BBchCtcAXtA-ghgjOPK_I9I3JQ6A3Huj58HHwq4ScfLiQWKhWDe7qPinGsNfdV31kNfN3e_-UvX28Wcvpqk0JBg="}],"persistentId":"0:1711623408410794%7031b2e6f9fd7ecd","ttl":2419200,"sent":"1711623408408","rawData":[144,100,24,211,44,137,162,184,201,92,144,234,7,197,77,100,149,56,136,47,103,51,248,229,49,83,203,100,91,191,11,118,151,111,8,55,72,14,228,148,33,72,59,211,84,32,112,20,184,10,11,117,105,172,109,21,163,165,149,84,19,5,18,7,105,183,142,104,117,239,128,161,91,56,75,226,113,36,1,140,53,168,206,233,83,118,186,247,86,217,163,59,57,183,47,194,105,155,78,88,176,203,166,85,189,101,158,207,145,115,178,125,3,150,177,90,107,58,247,207,174,225,24,81,255,198,170,76,88,3,193,213,204,173,241,188,189,165,32,250,205,243,75,195,200,166,63,52,253,199,109,18,21,30,145,50,220,163,34,19,152,148,190,75,234,218,135,238,200,253,113,100,172,46,225,22,189,246,221,5,48,188,238,208,34,145,104,91,225,200,112,59,227,155,184,163,37,103,48,161,164,72,71,49,136,89,218,122,139,20,43,120,163,161,175,136,89,152,67,148,198,7,83,107,134,132,176,203,239,178,247,178,153,110,214,223,237,251,20,184,125,243,18,8,40,105,135,133,51,190,243,0,160,218,12,150,219,10,95,67,37,25,192,43,245]},"keys":{"privateKey":"mxS1cWvYGFxJJS8RTsOFkW5VqRyL4k6mIH0K3V47aHQ","publicKey":"BF4--ivRFQqtqhVlEbK6HoBlznFZu9lcAwpps-IDwTpuzLgxUQkRpMsxTnOUUHdbZK4XmFFo3c4ArvxXOcJHDPc","authSecret":"UG5PMDBLZkw5WHVFWmp0Vw"}}')
+      .then((value) {
+    print("resutlt  :: $value");
   });
-
-
 }
+
 /// A longer lived native function, which occupies the thread calling it.
 ///
 /// Do not call these kind of native functions in the main isolate. They will
@@ -32,6 +30,7 @@ void main() {
 ///
 /// 1. Reuse a single isolate for various different kinds of requests.
 /// 2. Use multiple helper isolates for parallel execution.
+///
 Future<String> decryptMessage(String a) async {
   final SendPort helperIsolateSendPort = await _helperIsolateSendPort;
   final int requestId = _nextSumRequestId++;
@@ -42,7 +41,11 @@ Future<String> decryptMessage(String a) async {
   return completer.future;
 }
 
+//Will get the current path of the package to run it on pure dart.
+String _currentPackagePath = '';
+//dynamic lib name
 const String _libName = 'libfcm_decrypt';
+
 bool isRunningOnFlutter() {
   // Use reflection to check if the Flutter framework library is loaded
   try {
@@ -51,34 +54,45 @@ bool isRunningOnFlutter() {
     return false;
   }
 }
+
+/// This function will get the path of this package
+Future<String> getPackagePath() async {
+  String path = '';
+  var packageConfig = await findPackageConfig(Directory.current);
+  if (packageConfig != null) {
+    Package package = packageConfig.packages
+        .firstWhere((element) => element.name == 'flutter_fcm_decrypt');
+    path = package.packageUriRoot.path.toString().replaceAll('/lib/', '');
+  }
+  return path;
+}
+
 /// The dynamic library in which the symbols for [FlutterFcmDecryptBindings] can be found.
 final DynamicLibrary _dylib = () {
   String prefixPath = '';
-  if(!isRunningOnFlutter()){
-    prefixPath='';
+  if (!isRunningOnFlutter()) {
+    prefixPath = '$_currentPackagePath/macos/';
   }
   if (Platform.isMacOS || Platform.isIOS) {
-    if(Platform.version.contains('arm64')) {
+    if (Platform.version.contains('arm64')) {
       //TODO:: include './lib/' before lib name like  './lib/$_libName.dylib' for dart only run ..
-      return DynamicLibrary.open('$_libName'+'_64'+'.dylib');
+      return DynamicLibrary.open('$prefixPath$_libName' + '_64' + '.dylib');
     } else {
-      return DynamicLibrary.open('$_libName.dylib');
+      return DynamicLibrary.open('$prefixPath$_libName.dylib');
     }
-
   }
   if (Platform.isAndroid || Platform.isLinux) {
     return DynamicLibrary.open('$_libName.so');
   }
+  if (!isRunningOnFlutter()) {
+    prefixPath = '$_currentPackagePath\\windows\\';
+  }
   if (Platform.isWindows) {
     //TODO:: include '.\\windows\\' before lib name like  './lib/$_libName.dylib' for dart only run ..
-    return DynamicLibrary.open('$_libName.dll');
+    return DynamicLibrary.open('$prefixPath$_libName.dll');
   }
   throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
 }();
-
-/// The bindings to the native functions in [_dylib].
-final FlutterFcmDecryptBindings _bindings = FlutterFcmDecryptBindings(_dylib);
-
 
 /// A request to compute `sum`.
 ///
@@ -86,7 +100,6 @@ final FlutterFcmDecryptBindings _bindings = FlutterFcmDecryptBindings(_dylib);
 class _SumRequest {
   final int id;
   final String a;
-
 
   const _SumRequest(this.id, this.a);
 }
@@ -133,16 +146,20 @@ Future<SendPort> _helperIsolateSendPort = () async {
       }
       throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
     });
-
   // Start the helper isolate.
   await Isolate.spawn((SendPort sendPort) async {
+    /// The bindings to the native functions in [_dylib].
+    _currentPackagePath = await getPackagePath();
+    final FlutterFcmDecryptBindings _bindings =
+        FlutterFcmDecryptBindings(_dylib);
     final ReceivePort helperReceivePort = ReceivePort()
       ..listen((dynamic data) {
         // On the helper isolate listen to requests and respond to them.
         if (data is _SumRequest) {
-
-          Pointer<Utf8> result = _bindings.decrypt_message(data.a.toNativeUtf8());
-          final _SumResponse response = _SumResponse(data.id, result.cast<Utf8>().toDartString());
+          Pointer<Utf8> result =
+              _bindings.decrypt_message(data.a.toNativeUtf8());
+          final _SumResponse response =
+              _SumResponse(data.id, result.cast<Utf8>().toDartString());
           sendPort.send(response);
           return;
         }
